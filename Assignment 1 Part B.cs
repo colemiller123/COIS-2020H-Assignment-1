a12a16d6bd39cd23148efe213254f39294403616
colemiller123 <114594477+colemiller123@users.noreply.github.com>
2022-09-28 12:48:29 -0400

    Update Assignment 1 Part B.cs

--- Assignment 1 Part B.cs
+++ Assignment 1 Part B.cs
@@ -15,29 +15,29 @@ public class MyString {
         public char item;
         public Node next;
         // Constructor (2 marks)
-        public Node ( … ) { … }
+        //public Node ( … ) { … }
     }
 
-    private Node front; // Reference to the first (header) node
-    private int length; // Number of characters in MyString
+    //private Node front; // Reference to the first (header) node
+    //private int length; // Number of characters in MyString
     
     // Initialize with a header node an instance of MyString to the given character array A (4 marks)
-    public MyString (char[ ] A) { … }
+    //public MyString (char[ ] A) { … }
     
     // Using a stack, reverse this instance of MyString (6 marks)
-    public void Reverse ( ) { Stack<char> S; … }
+    //public void Reverse ( ) { Stack<char> S; … }
     
     // Return the index of the first occurrence of c in this instance; otherwise -1 (4 marks)
-    public int IndexOf(char c) { … }
+    //public int IndexOf(char c) { … }
     
     // Remove all occurrences of c from this instance (4 marks)
-    public void Remove (char c) { … }
+    //public void Remove (char c) { … }
     
     // Return true if obj is both of type MyString and the same as this instance; 
     // otherwise false (6 marks)
-    public override bool Equals (object obj) { … }
+    //public override bool Equals (object obj) { … }
     
     // Print out this instance of MyString (3 marks)
-    public void Print( ) { … }
+    //public void Print( ) { … }
 }
 
