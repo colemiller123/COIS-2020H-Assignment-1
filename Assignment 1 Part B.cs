377200df81ac49768266672e91e2dcb16a6387ba
colemiller123 <114594477+colemiller123@users.noreply.github.com>
2022-09-28 10:27:28 -0400

    Create Assignment 1 Part B.cs

---
+++ Assignment 1 Part B.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace AssigmentOne {
+    class Program {
+        static void Main(string[] args) {
+            //Run code here
+            Console.Write("Hello World");
+            
+        }
+    }
+}
+
+public class MyString {
+    private class Node {
+        public char item;
+        public Node next;
+        // Constructor (2 marks)
+        public Node ( … ) { … }
+    }
+
+    private Node front; // Reference to the first (header) node
+    private int length; // Number of characters in MyString
+    
+    // Initialize with a header node an instance of MyString to the given character array A (4 marks)
+    public MyString (char[ ] A) { … }
+    
+    // Using a stack, reverse this instance of MyString (6 marks)
+    public void Reverse ( ) { Stack<char> S; … }
+    
+    // Return the index of the first occurrence of c in this instance; otherwise -1 (4 marks)
+    public int IndexOf(char c) { … }
+    
+    // Remove all occurrences of c from this instance (4 marks)
+    public void Remove (char c) { … }
+    
+    // Return true if obj is both of type MyString and the same as this instance; 
+    // otherwise false (6 marks)
+    public override bool Equals (object obj) { … }
+    
+    // Print out this instance of MyString (3 marks)
+    public void Print( ) { … }
+}
+
