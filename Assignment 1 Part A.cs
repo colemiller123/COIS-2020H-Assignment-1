c005a02999bdcd552581671e3d40db39d66bee88
mattjhellard <114604816+mattjhellard@users.noreply.github.com>
2022-10-14 17:20:05 -0400

    Cleaned, Optimized
    Probably finished, should probably remove Demo class before handing in (not a requirement).
    -Cleaned and improved comments.
    -Cleaned code and slightly optimized Initialize method.

--- Assignment 1 Part A.cs
+++ Assignment 1 Part A.cs
@@ -1,3 +1,14 @@
+/*
+ * COIS 2020H | Assignment 1 - Part A
+ * Group Members (alphabetical): Cole Miller, Jesse Laframboise, Matthew Hellard
+ * 
+ * ----
+ * Easiest way to run in Visual Studio:
+ * Create new Console App (.NET Framework)
+ * Clear new .cs file of contents
+ * Copy & Paste this code into new .cs file
+ * ----
+ */
 using System;
 
 public enum TColor { WHITE, BLACK };
@@ -16,18 +27,22 @@ public class Square
 
 public class Puzzle
 {
-    private Square[,] grid; // treated as [X,Y], so [1,4] would mean row 1 column 4, this can be changed relatively easily and doesn't really matter as long as it's treated consistently
+    private Square[,] grid; // treated as [X,Y], so [1,4] would mean row 1 column 4, should be treated consistently
     private int N;
 
     // Create an NxN crossword grid of WHITE squares (4 marks)
     public Puzzle(int N)
     {
+        if (N < 0)
+        {
+            throw new Exception("Cannot construct Puzzle with negative value");
+        }
         this.N = N;
         grid = new Square[this.N, this.N]; //field used to eliminate chance of field being inaccurate
         for (int ix = 0; ix < this.N; ix++)
-        {//populating grid with actual Squares, without this grid is nothing but null [replace with foreach?]
+        {
             for (int iy = 0; iy < this.N; iy++)
-            {
+            { //populating grid with actual squares
                 grid[ix, iy] = new Square(); //see Square constructor
             }
         }
@@ -36,36 +51,39 @@ public class Puzzle
     // Randomly initialize a crossword grid with M black squares (5 marks)
     public void Initialize(int M)
     {
-        Random rand = new Random(); //Random class used to generate random ints
-        bool full = false; //control variable for tracking if crossword is full of black squares (full==true) or not (full==false)
-        int[,] vacancies = new int[N * N, 2]; //every coordinate that's not black, columns are coordinates, x on top, y on bottom, row value will be used for listing coords.
-        int vacanciesCount = 0; //tracking how many coordinates populate vacancies (array itself set to maximum possible value)
-        for (int ix = 0; ix < N; ix++) //manages column by column, so x managed by outer loop
+        if (M < 0)
         {
-            for (int iy = 0; iy < N; iy++) //y managed by inner loop
+            throw new Exception("Cannot initialize Puzzle with negative value");
+        }
+        //building list of available coordinates
+        int[,] vacancies = new int[N * N, 2]; //every coordinate that's not black, columns are coordinates, x on top, y on bottom.
+        int vacanciesCount = 0; //tracking how many coordinates populate vacancies (array itself set to maximum possible value)
+        for (int ix = 0; ix < N; ix++)
+        {
+            for (int iy = 0; iy < N; iy++)
             {
                 if (grid[ix, iy].Color == TColor.WHITE) //if current coordinate is white
                 {
-                    vacancies[vacanciesCount, 0] = ix; //uses ix as x coordinate, as vacancies is being build, vacanciesCount acts as good index for where to add coords.
-                    vacancies[vacanciesCount, 1] = iy; //uses iy as y coordinate
-                    vacanciesCount++; // has to be updated last so that initial value (0) can be used to populate vacancies
+                    vacancies[vacanciesCount, 0] = ix;
+                    vacancies[vacanciesCount, 1] = iy;
+                    vacanciesCount++; //has to be updated last so that initial value can be used to populate vacancies
                 }
             }
         }
-        for (int i = 0; i < M && full == false; i++)
+        //placing black squares and updating list
+        Random rand = new Random(); //Random class used to generate random ints
+        int index = 0;
+        while (index < M && index < N * N) //while index is less than M AND N^2, considering N lets users "overfill" the grid without consequence
         {
-            if (vacanciesCount > 0) //if vacanciesCount<=0 then all squares are black and no new square can be turned black
+            int listPicker = rand.Next(0, vacanciesCount); //picking random coord from list of available coords to make black
+            grid[vacancies[listPicker, 0], vacancies[listPicker, 1]].Color = TColor.BLACK; //making square at randomly chosen coord from vacancies black
+            for (int i = listPicker; i + 1 < vacanciesCount; i++) //updating vacancies to remove used item and reorganize for reuse
             {
-                int listPicker = rand.Next(0, vacanciesCount); //picking random coord from list of available coords to make black
-                grid[vacancies[listPicker, 0], vacancies[listPicker, 1]].Color = TColor.BLACK; //making square at randomly chosen coord black
-                for (int i1 = listPicker; i1 + 1 < vacanciesCount; i1++) //updating vacancies to remove used item and reorganize for reuse
-                {
-                    vacancies[i1, 0] = vacancies[i1 + 1, 0]; //moving everything down to fill used index
-                    vacancies[i1, 1] = vacancies[i1 + 1, 1]; //moving everything down to full used index
-                }
-                vacanciesCount--; //updating to represent that a value has been used and can't be used again
+                vacancies[i, 0] = vacancies[i + 1, 0]; //moving everything down to fill used index
+                vacancies[i, 1] = vacancies[i + 1, 1]; //moving everything down to full used index
             }
-            else { full = true; } //if vacanciesCount is 0 then initialization is finished, the program will still compile without this control, this just reduces unnecessary processing  
+            vacanciesCount--; //updating to represent that a value has been used and can't be used again
+            index++;
         }
     }
 
@@ -76,23 +94,21 @@ public class Puzzle
         for (int iy = 0; iy < N; iy++)
         {
             for (int ix = 0; ix < N; ix++)
-            {   //considers acrosses first, idk why but it feels right
-                //if on left end of grid or to right of black square, and space is ample
-                if (ix + 1 < N
+            {   //goes row by row so that numbers will progress row by row
+                //across considerations
+                if (ix + 1 < N //if on left end of grid or to right of black square, and space is ample
                     && (ix > 0 && grid[ix - 1, iy].Color == TColor.BLACK || ix == 0)
                     && grid[ix, iy].Color == TColor.WHITE
                     && grid[ix + 1, iy].Color == TColor.WHITE)
-                //&& grid[ix + 2, iy].Color == TColor.WHITE)
                 {
                     grid[ix, iy].Number = number;
                     number++;
                 }
-                //downs considerations, fundamentally same as across but also doesn't overwrite existing numbers
-                if (grid[ix, iy].Number == -1 && iy + 1 < N
+                //down considerations, doesn't overwrite existing numbers because an existing number would be for across and would never be overwritten by down 
+                if (iy + 1 < N && grid[ix, iy].Number == -1 //if number is unused and on top end of grid or below black square, and space is ample
                     && (iy > 0 && grid[ix, iy - 1].Color == TColor.BLACK || iy == 0)
                     && grid[ix, iy].Color == TColor.WHITE
                     && grid[ix, iy + 1].Color == TColor.WHITE)
-                //&& grid[ix, iy + 2].Color == TColor.WHITE)
                 {
                     grid[ix, iy].Number = number;
                     number++;
@@ -104,48 +120,50 @@ public class Puzzle
     // Print out the numbers for the Across and Down clues (in order) (4 marks)
     public void PrintClues()
     {
-        int[] Across = new int[N * N]; //a queue would be better for this but array that is definitely big enough is easier 
-        int acrossCount = 0; //tracking size of inhabited array
-        int[] Down = new int[N * N];
-        int downCount = 0;
+        //building complete lists of across and down clues before printing anything
+        int[] Across = new int[N * N]; //array to store acrosses
+        int acrossCount = 0; //tracking size of inhabited across array
+        int[] Down = new int[N * N]; //array to store downs
+        int downCount = 0; //tracking size of inhabited down array
         for (int iy = 0; iy < N; iy++)
-        { //goes row through row
+        {
             for (int ix = 0; ix < N; ix++)
             {
-                if (grid[ix, iy].Number != -1) //only checks numbered squares
-                { //has to check validity of across and down again in order to determine if the number is for down or across or both, it feels like this should be avoidable but idk
-                    if (ix + 1 < N
+                if (grid[ix, iy].Number != -1) //only checks numbered squares. Because numbers will only end up on valid squares, checks past here don't need to check current square for anything
+                { //has to check validity of across and down again in order to determine if the number is for down or across or both
+                    //across
+                    if (ix + 1 < N //if on left end of grid or to right of black square, and space is ample
                     && (ix > 0 && grid[ix - 1, iy].Color == TColor.BLACK || ix == 0)
                     && grid[ix + 1, iy].Color == TColor.WHITE)
-                    //&& grid[ix + 2, iy].Color == TColor.WHITE)
                     {
-                        Across[acrossCount] = grid[ix, iy].Number; //occupying arrays with appropriate clue numbers
+                        Across[acrossCount] = grid[ix, iy].Number; //occupying array with appropriate clue numbers
                         acrossCount++;
                     }
-                    if (iy + 1 < N //same as across but down
+                    //down
+                    if (iy + 1 < N //if number is unused and on top end of grid or below black square, and space is ample
                     && (iy > 0 && grid[ix, iy - 1].Color == TColor.BLACK || iy == 0)
                     && grid[ix, iy + 1].Color == TColor.WHITE)
-                    //&& grid[ix, iy + 2].Color == TColor.WHITE)
                     {
-                        Down[downCount] = grid[ix, iy].Number;
+                        Down[downCount] = grid[ix, iy].Number; //occupying array with appropriate clue numbers
                         downCount++;
                     }
                 }
             }
-        } //printing
+        } 
+        //printing
         int scale = Convert.ToInt32(Convert.ToString(N * N * 2).Length) + 5; //scale used to determine spacing when lining up number columns
         Console.Write("\nAcross:");
         for (int i = 0; i < scale; i++)
         {
-            Console.Write(' '); //scale based spacing, should give enough horizontal room for biggest clue number without messing up spacing
+            Console.Write(' '); //scale based spacing between list titles, should give enough horizontal room for biggest clue number without messing up spacing
         }
         Console.WriteLine("Down:");
         for (int i = 0; i < downCount || i < acrossCount; i++) //repeats until across and down are both exhausted
         {
-            if (i < acrossCount) { Console.Write(Across[i]); } else { Console.Write(' '); } //only prints if there are across clues left, otherwise prints a blank space to maintain spacing(bandaid fix)
-            for (int spacing = 0; spacing < scale + 7 - Convert.ToInt32(Convert.ToString(Across[i]).Length); spacing++) //determining amount of spacing to add, +7 accounts for length of "Across:" string
-            { //increase distance between down and across
-                Console.Write(' '); //spacing
+            if (i < acrossCount) { Console.Write(Across[i]); } else { Console.Write(' '); } //prints across clues until exhausted, then prints blanks to maintain spacing for down list
+            for (int spacing = 0; spacing < scale + 7 - Convert.ToInt32(Convert.ToString(Across[i]).Length); spacing++) //adding appropriate spacing, +7 accounts for length of "Across:" string
+            {
+                Console.Write(' '); //spacing between actual contents of lists
             }
             if (i < downCount) { Console.Write(Down[i]); } //only prints if there are down clues left
             Console.WriteLine();
@@ -153,10 +171,10 @@ public class Puzzle
 
     }
     // Print out the crossword grid including the BLACK squares and clue numbers (5 marks)
-    public void PrintGrid() //inverted colourwise as white blanks look better on black background (console)
-    { // breaks when boxes exceed console window, I cannot even begin to fathom a fix
+    public void PrintGrid() //inverted colourwise as white black squares look better on black background (console)
+    { // breaks when boxes exceed console window, no reasonable fix as it depends on console window size and display size, neither of which can be reasonably known by this program let alone dealt with
         Console.WriteLine("*Note: Colours inverted*");
-        //scale grid by largest possible clue number (square of size doubled due to downs and ups), could use actual largest clue number, but that would take much longer *maybe implement later*
+        //scale grid by largest possible clue number (square of size doubled due to downs and ups)
         int scale = Convert.ToInt32(Convert.ToString(N * N * 2).Length) * 2; //scale=number of digits in 2xN^2,x2. Second double is to let nums go in top left of box 
         linebreak();//see internal linebreak method
         for (int iy = 0; iy < N; iy++)
@@ -187,10 +205,11 @@ public class Puzzle
                     }
                 }
                 Console.Write('|'); //end row of box with column divider (|)
-            }
-            for (int vertical = 0; vertical < Math.Round(Math.Sqrt(scale)); vertical++) //adds verticality to each box, without this, every box is one line tall, the amount of verticality is somewhat dependent on width
+            } // at this point top row of current square of current row has been printed
+            //adding verticality to each box, without this, every box is one line tall, the amount of verticality is somewhat dependent on width (N)
+            for (int vertical = 0; vertical < Math.Round(Math.Sqrt(scale)); vertical++) 
             {
-                Console.WriteLine(); //next line
+                Console.WriteLine();
                 Console.Write('|');//front divider for row
                 for (int ix = 0; ix < N; ix++)
                 {
@@ -220,9 +239,10 @@ public class Puzzle
             Console.WriteLine();
         }
     }
+
     // Return true if the grid is symmetric (à la New York Times); false otherwise (4 marks)
-    public bool Symmetric() //hasn't been adequately tested 
-    { //further examples show that symmetry is defined as the crossword being reversible (crossword reads the same forward and backward)
+    public bool Symmetric()
+    { //NYT symmetry seems to be defined as being reversible (the same forwards and backwards)
         for (int iy = 0; iy < N; iy++)
         {
             for (int ix = 0; ix < N; ix++)
@@ -233,23 +253,23 @@ public class Puzzle
                 }
             }
         }
-        return true; //should only get here if all points match the desired symmetry
+        return true; //should only get here if all points fulfill the required symmetry
     }
 }
 
-public class Demo //for actually doing stuff
+public class Demo //get rid of this class before handing in? (not part of assignment)
 {
     public static void Main()
     {
         while (true) //so you can restart without closing and starting up again
         {
             Console.Write("Input size of crossword :");
-            Puzzle crossword = new Puzzle(Convert.ToInt32(Console.ReadLine())); //wrap in try-catch loop later
+            Puzzle crossword = new Puzzle(Convert.ToInt32(Console.ReadLine()));
 
             //Puzzle crossword = new Puzzle(3); //for automatic input
 
             Console.Write("Input number of black spaces :");
-            crossword.Initialize(Convert.ToInt32(Console.ReadLine())); //wrap in try-catch loop later
+            crossword.Initialize(Convert.ToInt32(Console.ReadLine()));
 
             //crossword.Initialize(4); //for automatic input
 
